deb8975b36f098a83e5d974e944225c0a9d8ba55
Aibarret <83474314+ZplaxAgogNonZeep@users.noreply.github.com>
2022-07-07 15:02:11 -0400

    Began work on Audio Playing system and put together PoC Picture game framework
    Shelved Font work for the time being in favor of getting the PoC Finished in a timely manner

---
+++ KtunaxaGame/AudioManager.gd
@@ -0,0 +1,8 @@
+extends Node
+
+# Manages various audio track signals. 
+
+func startTrack(trackIndex : int, isLooping : bool, toReplace:=0):
+	# Takes a track and decides what to do with it with the given variables.
+	# toReplace variable will end any playing track at the given index with 0 as null
+	pass
--- KtunaxaGame/Game.gd
+++ KtunaxaGame/Game.gd
@@ -1,5 +1,29 @@
 extends Node2D
 
-func _ready():
-	pass # Replace with function body.
+var mainMenu = preload("res://MainMenu/MainMenu.tscn")
+var pictureGame = preload("res://PictureGame/PictureGame.tscn")
 
+func _ready():
+	$GameManager.add_child(mainMenu.instance())
+
+func changeGame(game : int):
+	# Changes the node in the GameManager using a given int as reference
+	# 0 = Main Menu
+	# 1 = Picture Game
+	# 2 = Spelling Game
+	if $GameManager.get_children() != []:
+		while $GameManager.get_child_count() != 0:
+			$GameManager.get_child(0).queue_free()
+	yield(get_tree(), "idle_frame")
+	
+	match game:
+		0:
+			$GameManager.add_child(mainMenu.instance())
+		1:
+			$GameManager.add_child(pictureGame.instance())
+		_:
+			print("changeGame() Could not find appropriate scene with given input of " + str(game))
+	
+
+func playAudio():
+	pass
