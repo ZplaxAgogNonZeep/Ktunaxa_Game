0920fc474cc79f614bf6ad1bea663d0e177c5606
Aibarret <83474314+ZplaxAgogNonZeep@users.noreply.github.com>
2022-07-22 14:36:53 -0400

    Implemented LetterKey system and finished base WordGenerator
    Still need to:
    -Center word
    -implement Spaces

--- KtunaxaGame/WordSystem/WordGenerator.gd
+++ KtunaxaGame/WordSystem/WordGenerator.gd
@@ -2,10 +2,13 @@ extends Node2D
 
 var letter = preload("res://WordSystem/Letter.tscn")
 
+func _ready():
+	generateWord("17>24>21")
+
 func generateWord(wordCode : String):
 	# Starts the word generation process, takes a string of numbers that will make
 	# up the word to be generated then creates Sprite nodes in the form of the word
-	var posn = $Position2D.position
+	var posn = Vector2(0,0)
 	
 	var letterList : Array = wordCode.split(">")
 	
@@ -13,9 +16,21 @@ func generateWord(wordCode : String):
 		var newLetter = letter.instance()
 		newLetter.texture = getLetter(letterList[i])
 		posn.x = posn.x + 48
-		newLetter.position.x = posn.x
+		newLetter.position = Vector2(posn.x, posn.y)
 		$WordManager.add_child(newLetter)
+	
+	posn.x += 48
+	
+	
+	$WordManager.position = $Position2D.position
+	$Position2D.position = posn
+	
+	$WordManager.global_position.x = $Position2D.global_position.x - $WordManager.global_position.x
+	print($WordManager.global_position)
 
 
 func getLetter(index : String):
-	return load("res://Assets/Letters/" + index)
+	if int(index) > 0:
+		return load("res://Assets/Letters/" + index + ".png")
+	else:
+		return null
